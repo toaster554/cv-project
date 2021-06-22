03016137499a40c7fadd150ab8c7bf86edfa0ed5
Nathan Nguyen <nguyenbut24@gmail.com>
2021-06-22 13:31:53 -0400

    Updates

---
+++ precache-manifest.cb8d1d3faf395fb0e301b2ccc2aaff09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d310d5f2043e843c84e2",
+    "url": "/cv-project/static/css/main.8648454b.chunk.css"
+  },
+  {
+    "revision": "d310d5f2043e843c84e2",
+    "url": "/cv-project/static/js/main.b2ca4279.chunk.js"
+  },
+  {
+    "revision": "750bb09279145c08b516",
+    "url": "/cv-project/static/js/runtime~main.f194feb3.js"
+  },
+  {
+    "revision": "9cb40a194b3408c88cbd",
+    "url": "/cv-project/static/js/2.d7fe365a.chunk.js"
+  },
+  {
+    "revision": "e4e22333c5ed5201ddde418046b3c4be",
+    "url": "/cv-project/index.html"
+  }
+];
